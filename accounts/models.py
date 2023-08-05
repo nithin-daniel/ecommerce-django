03f1291564078318a839b1f85935a3623ae94ddc
nithindaniel1 <nithindaniel2018@gmail.com>
2023-08-05 15:29:13 +0530

    added custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomAccountManager(BaseUserManager):
+    def create_user(self,email,password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self,email,password,**other_fields):
+
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_superuser',True)
+        other_fields.setdefault('is_active',True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
+        
+        return self.create_user(email,password, **other_fields)
+
+    
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(_('Email Address'),unique=True)
+    password = models.CharField(_('Password Field'),max_length = 25,unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
