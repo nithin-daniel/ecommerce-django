761386ad51b76704f8ca104b00cb051f18ecd90a
nithindaniel1 <nithindaniel2018@gmail.com>
2023-08-09 23:29:04 +0530

    listed all product in homepage

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from store.models import Product
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    all_product = Product.objects.all()
+    context = {
+        'all_product':all_product
+    }
+    return render(request,'home.html',context)
