211727b5bbf73e3741533fb221aa48a9cc991e4f
nithindaniel1 <nithindaniel2018@gmail.com>
2023-08-11 22:00:12 +0530

    cart model added

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class cart(models.Model):
+    product_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='cart_image/', default='media/no-image-default.png')
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.product_name
