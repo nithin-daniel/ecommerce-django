b2f413dcb67b0d519c90360b47edf29e7c0fbde3
nithindaniel1 <nithindaniel2018@gmail.com>
2023-09-16 02:19:03 +0530

    updated urls in project

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -29,4 +29,9 @@ urlpatterns = [
     path('payment/',include('payments.urls')),
 ]
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
